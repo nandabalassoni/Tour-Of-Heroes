5c314796a6ea85b09ef5cf8e9b373a780c2866ef
nandabalassoni <mariafernandabalassoni@hotmail.com>
2021-03-20 20:06:07 -0300

    adicionando metodo que atribui o heroi clicado do modelo ao do componente

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'; //importando Hero
+import { HEROES } from '../mock-heroes'; // importando o mock-heroes, lista com os heróis
 
 @Component({
   selector: 'app-heroes',
@@ -8,6 +9,13 @@ import { Hero } from '../hero'; //importando Hero
 })
 
 export class HeroesComponent implements OnInit {  //refatorando Hero e inicializando com id e name
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   hero: Hero = {
     id: 1,
