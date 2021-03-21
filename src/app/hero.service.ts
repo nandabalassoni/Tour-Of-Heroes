b2acaa4390d6d7974b8e328601692ddac9f83470
nandabalassoni <mariafernandabalassoni@hotmail.com>
2021-03-21 12:59:54 -0300

    adicionando método que permite um pedido http sem ter que alterar o componente que o chama

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,10 +16,18 @@ export class HeroService {
     return of(HEROES);
   } // -> O método envia mensagem quando os heróis forem buscados
 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
   constructor(private messageService: MessageService) { } /*modificando o construtor com um parâmetro que declara uma messageService propriedade privada.  O Angular injetará o singleton MessageServicenessa propriedade ao criar o HeroService. */
 }
 
 /* Um HeroServiceque todas as classes de aplicativo podem usar para obter heróis.
 Em vez de criar esse serviço com new, você dependerá da injeção de dependência Angular para injetá-lo no HeroesComponentconstrutor.
 Os serviços são uma ótima maneira de compartilhar informações entre classes que não se conhecem */
+
+
 
