4c2bdf0a580e89bab67ec8a287505b378801744c
nandabalassoni <mariafernandabalassoni@hotmail.com>
2021-03-21 12:57:56 -0300

    adicionando importações, construtor e métodos para serviço, localização e roteamento

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Hero } from '../hero';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -9,11 +14,34 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero: Hero;  //Este componente simplesmente recebe um objeto herói por meio de sua heropropriedade e o exibe
+  hero: Hero;
 
-  constructor() { }
+  //@Input() hero: Hero;  //Este componente simplesmente recebe um objeto herói por meio de sua heropropriedade e o exibe
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
+
+  /* O ActivatedRoutecontém informações sobre a rota para esta instância do HeroDetailComponent. Este componente está interessado no pacote de parâmetros da rota extraído da URL. O parâmetro "id" é o iddo herói a ser exibido.
+
+O HeroServiceobtém dados do herói do servidor remoto e este componente os usará para exibir o herói .
+
+O locationé um serviço Angular para interagir com o navegador. Você o usará mais tarde para navegar de volta à visualização que navegou aqui. */
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
