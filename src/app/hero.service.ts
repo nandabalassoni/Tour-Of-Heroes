b00379e14dd997afe6e025e92928937a61cd71d5
nandabalassoni <mariafernandabalassoni@hotmail.com>
2021-03-21 16:08:23 -0300

    fazendo importações e adicionando metodos no construtor para adição, alteração e exclusão de algum heroi

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero'; // formatação onde mostra id e nome dos herois
-import { HEROES } from './mock-heroes'; //lista com os herois e dados deles
+//import { HEROES } from './mock-heroes'; //lista com os herois e dados deles
 import { Observable, of } from 'rxjs'; // é uma das principais classes da biblioteca rxjs.
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }) // cabeçalho especial nas solicitações de salvamento HTTP.
+};
 
 @Injectable({
   providedIn: 'root'
@@ -10,24 +16,131 @@ import { MessageService } from './message.service';
 
 export class HeroService {
 
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
-  } // -> O método envia mensagem quando os heróis forem buscados
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  getHero(id: number): Observable<Hero> {
+  /*getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }*/
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** GET heroes from the server */
+
+  /* Os HeroServicemétodos acessarão o fluxo de valores observáveis ​​e enviarão uma mensagem (via log()) para a área de mensagem na parte inferior da página.
+
+Eles farão isso com o tapoperador RxJS , que olha os valores observáveis, faz algo com esses valores e os repassa. A tapchamada de volta não afeta os próprios valores. */
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
-  constructor(private messageService: MessageService) { } /*modificando o construtor com um parâmetro que declara uma messageService propriedade privada.  O Angular injetará o singleton MessageServicenessa propriedade ao criar o HeroService. */
+  /** GET hero by id. Return `undefined` when id not found */
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url)
+      .pipe(
+        map(heroes => heroes[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? `fetched` : `did not find`;
+          this.log(`${outcome} hero id=${id}`);
+        }),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
+
+  /** GET hero by id. Will 404 if id not found */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
+  /* O método retorna imediatamente com uma matriz vazia se não houver um termo de pesquisa. O resto se assemelha muito getHeroes(). A única diferença significativa é o URL, que inclui uma string de consulta com o termo de pesquisa. */
+
+  //////// Save methods //////////
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  //constructor(private messageService: MessageService) { } /*modificando o construtor com um parâmetro que declara uma messageService propriedade privada.  O Angular injetará o singleton MessageServicenessa propriedade ao criar o HeroService. */
 }
 
 /* Um HeroServiceque todas as classes de aplicativo podem usar para obter heróis.
 Em vez de criar esse serviço com new, você dependerá da injeção de dependência Angular para injetá-lo no HeroesComponentconstrutor.
 Os serviços são uma ótima maneira de compartilhar informações entre classes que não se conhecem */
+
+
 
 
 
